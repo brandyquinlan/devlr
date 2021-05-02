48844825568608c4f9a8a2f190007b4b2de465b8
Kat Buchanan <katrbuch@gmail.com>
2021-05-02 09:48:26 -0500

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/index.css';
 
@@ -9,9 +10,9 @@ function App() {
     <Router>
       <div className="container">
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Signup} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/home" component={Home} />
         </Switch>
       </div>
     </Router>

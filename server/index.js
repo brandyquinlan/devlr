3ed26a3b822ea4ae07fed4dd8a04c6b70863b8e6
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-01 20:37:24 -0500

    added boiler plate routing on server side, as well as set up boiler plate expres router

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+require('./api')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server online and listening on ${PORT}`);
+});

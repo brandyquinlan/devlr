d51811a78bdaa3f150a2ededdc1250b903f54eec
Kat Buchanan <katrbuch@gmail.com>
2021-05-01 19:54:17 -0500

    sidebar component

---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,33 @@
+const Sidebar = () => {
+  return (
+    <div id="side-nav">
+      <ul className="list-group">
+        <li className="m-3">
+          <a href="/home">
+            <i className="material-icons">home</i>
+            Home
+          </a>
+        </li>
+        <li className="m-3">
+          <i className="material-icons">groups</i>
+          Browse Users
+        </li>
+        <li className="m-3">
+          <i className="material-icons">dashboard</i> Create Profile
+        </li>
+        <li className="m-3">
+          <i className="material-icons">manage_accounts</i>
+          Account
+        </li>
+        <li className="m-3">
+          <a href="/logout">
+            <i className="material-icons">keyboard_return</i>
+            Logout
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;

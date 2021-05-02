5ff838b5503a8c697043eb0e21ec38778474c69f
Kat Buchanan <katrbuch@gmail.com>
2021-05-01 20:25:00 -0500

    feed pages

---
+++ client/src/Components/NavTabs/Activity.js
@@ -0,0 +1,9 @@
+const Activity = () => {
+    return (
+        <div className="tab gradient">
+            <p class="lead">Perform some actions to get more notifications here!</p>
+        </div>
+    )
+}
+
+export default Activity
---
+++ client/src/Components/NavTabs/Explore.js
@@ -0,0 +1,9 @@
+const Explore = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Explore

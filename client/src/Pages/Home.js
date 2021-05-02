9c4e890ee9501cefe70d0554ebe395632faea3c9
Kat Buchanan <katrbuch@gmail.com>
2021-05-01 20:25:40 -0500

    placed new nav

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,15 +1,13 @@
+// import { useState } from 'React';
 import Sidebar from '../Components/Sidebar';
-import NavTabs from '../Components/NavTabs';
-import Tab from '../Components/Tab';
+import Navbar from '../Components/Navbar';
 
 const Home = () => {
   return (
     <div className="d-flex flex-row align-items-center justify-content-around">
       <Sidebar />
       <div className="d-flex flex-column align-items-left">
-        <NavTabs />
-        <Tab title="My Background" content="Figuring out how to pass the info we want."/>
-        <Tab title="My Languages" />
+        <Navbar />
       </div>
     </div>
   );

4f4c13c6c0e4ae3332200877a6c44107e1a237b0
Kat Buchanan <katrbuch@gmail.com>
2021-05-01 22:45:44 -0500

    resized headings

--- client/src/Components/Tab.js
+++ client/src/Components/Tab.js
@@ -2,7 +2,7 @@ const Tab = ({ title, content }) => {
   return (
     <div className="tab gradient">
       <div className="d-flex flex-row align-items-center justify-content-between">
-        <h4>{title}</h4>
+        <h5>{title}</h5>
         <span className="material-icons">expand_more</span>
       </div>
       <div className="separator mb-3"></div>

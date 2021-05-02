bef44a2b85494d6b7e8634979db1b7cf219fdd97
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-02 07:27:14 -0500

    working on configuring eslint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,9 +8,9 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-require('./api')(app);
+require("./api")(app);
 
 app.listen(PORT, () => {
     console.log(`Server online and listening on ${PORT}`);

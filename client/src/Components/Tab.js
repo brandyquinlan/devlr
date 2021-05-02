54b920c01c26fe677dff27a29160e7791635e9cb
Kat Buchanan <katrbuch@gmail.com>
2021-05-01 21:07:30 -0500

    positioning layout

--- client/src/Components/Tab.js
+++ client/src/Components/Tab.js
@@ -1,14 +1,14 @@
 const Tab = (props) => {
-    return (
-        <div className="tab gradient">
-           <div className="d-flex flex-row align-items-center justify-content-between">
-           <h4>{props.title}</h4>
-           <span className="material-icons">expand_more</span>
-           </div>
-           <div className="separator mb-3"></div>
-           <div className="tab">{props.content}</div>
-        </div>
-    )
-}
+  return (
+    <div className="tab gradient" style={{width: '400px'}}>
+      <div className="d-flex flex-row align-items-center justify-content-between">
+        <h4>{props.title}</h4>
+        <span className="material-icons">expand_more</span>
+      </div>
+      <div className="separator mb-3"></div>
+      <div>{props.content}</div>
+    </div>
+  );
+};
 
-export default Tab
+export default Tab;

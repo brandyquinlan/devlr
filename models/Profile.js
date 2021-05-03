3e3a45bb9fd563ca0698ae88e0ac8bac984e6379
vinitha <selvivini@gmail.com>
2021-05-03 13:39:07 -0500

    added ref user id to populate user id for the profile

--- models/Profile.js
+++ models/Profile.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
+    //adding the ref to user objectId to associate the profile with the user's id
+    user:{
+     type: Schema.Types.ObjectId,
+     ref: 'user'
+    },
     name: {
         type: String,
         required: true

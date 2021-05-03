7153cf46a90e61060e8943699ca68f306d4f6519
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-03 15:54:35 -0500

    merging changes from circleci-work

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-=======
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> circleci-work
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the home of devlr
-        </p>
+        <p>Welcome to the home of devlr</p>
       </header>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 const express = require('express')
 const path = require('path')
-=======
-const express = require("express");
-const path = require("path");
->>>>>>> circleci-work
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
-<<<<<<< HEAD
 app.use(
   express.urlencoded({
     extended: true,
@@ -19,15 +13,6 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 require('./api')(app)
-=======
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-require("./api")(app);
->>>>>>> circleci-work
 
 app.listen(PORT, () => {
   console.log(`Server online and listening on ${PORT}`)

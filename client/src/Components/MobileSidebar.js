fd88dacf87d5390d09e1eec11fba5102f0f87330
Kat Buchanan <katrbuch@gmail.com>
2021-05-03 21:24:15 -0500

    mobile sidebar nav

---
+++ client/src/Components/MobileSidebar.js
@@ -0,0 +1,28 @@
+const MobileSidebar = () => {
+    return (
+      <div id="side-nav">
+        <h2>devlr</h2>
+        <ul className="list-group">
+          <li className="m-3">
+            <a href="/home">
+              <i className="material-icons">home</i>
+            </a>
+          </li>
+          <li className="m-3">
+            <i className="material-icons">groups</i>
+          </li>
+          <li className="m-3">
+            <i className="material-icons">dashboard</i></li>
+          <li className="m-3">
+            <i className="material-icons">manage_accounts</i></li>
+          <li className="m-3">
+            <a href="/logout">
+              <i className="material-icons">keyboard_return</i></a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default MobileSidebar;
+  

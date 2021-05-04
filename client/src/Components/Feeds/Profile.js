bbaff68258665efa55c621406f3912b299e05e1d
Kat Buchanan <katrbuch@gmail.com>
2021-05-03 21:19:34 -0500

    rm content props

--- client/src/Components/Feeds/Profile.js
+++ client/src/Components/Feeds/Profile.js
@@ -3,9 +3,9 @@ import Tab from '../Tab';
 const Profile = () => {
   return (
     <>
-      <Tab title="My Background" content="figuring it out" />
-      <Tab title="My Languages" content="more content with prop types?" />
-      <Tab title="My Projects" content="la la la" />
+      <Tab title="My Background"/>
+      <Tab title="My Languages"/>
+      <Tab title="My Projects"/>
     </>
   );
 };

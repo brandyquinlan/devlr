c212094c05734e38a248e809ea9c4cf3fdaae157
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-04 22:52:59 -0500

    added boilerplate route for adding a new user

--- server/api.js
+++ server/api.js
@@ -7,9 +7,9 @@ module.exports = (app) => {
     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
   })
 
-  app.post('/api/newPost', async (request, response) => {
+  app.post('/api/newPost', async ({ body }, response) => {
     // assuming that the post will be send in its object from from the client
-    const post = request.body
+    const post = body
 
     // we simply attempt to store it to the database
     try {
@@ -20,7 +20,23 @@ module.exports = (app) => {
       response.sendStatus(400)
       // Deatiled error loging will be important for everyones sake in the dev process
       throw new Error(
-        `Error saving Post in databse --- server/api.js --- ERROR: ${error}`,
+        `Error adding Post to databse --- server/api.js --- ERROR: ${error}`,
+      )
+    }
+  })
+
+  app.post('/api/newUser', ({ body }, response) => {
+    // Again assuming that information is being sent in its object form from the client
+    const user = body
+
+    try {
+      db.User.create(user).then((res) => {
+        response.send(res)
+      })
+    } catch (error) {
+      response.sendStatus(400)
+      throw new Error(
+        `Error adding new User in databse --- server/api.js --- ERROR: ${error}`,
       )
     }
   })

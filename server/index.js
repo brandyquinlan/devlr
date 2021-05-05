2b2691817bcae0c574a214ba71cee931d93fa858
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-04 22:12:54 -0500

    added mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -13,6 +14,13 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 require('./api')(app)
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/database', {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
 
 app.listen(PORT, () => {
   console.log(`Server online and listening on ${PORT}`)

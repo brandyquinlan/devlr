b946bfef48821bd119bd2660cf8eac51748e7345
Kat Buchanan <katrbuch@gmail.com>
2021-05-05 09:21:11 -0500

    cleaned up syntax

--- client/src/Components/MobileSidebar.js
+++ client/src/Components/MobileSidebar.js
@@ -1,28 +1,35 @@
-const MobileSidebar = () => {
-    return (
-      <div id="mobile-side-nav">
-        <div className="circle" style={{width: '44px', height: '44px', background: 'linen'}}></div>
-        <ul className="list-group">
-          <li className="m-3">
-            <a href="/home">
-              <i className="material-icons">home</i>
-            </a>
-          </li>
-          <li className="m-3">
-            <i className="material-icons">groups</i>
-          </li>
-          <li className="m-3">
-            <i className="material-icons">dashboard</i></li>
-          <li className="m-3">
-            <i className="material-icons">manage_accounts</i></li>
-          <li className="m-3">
-            <a href="/logout">
-              <i className="material-icons">keyboard_return</i></a>
-          </li>
-        </ul>
-      </div>
-    );
-  };
-  
-  export default MobileSidebar;
-  
+import React from 'react'
+
+function MobileSidebar() {
+  return (
+    <div id="mobile-side-nav">
+      <div
+        className="circle"
+        style={{ width: '44px', height: '44px', background: 'linen' }}
+      ></div>
+      <ul className="list-group">
+        <li className="m-3">
+          <a href="/home">
+            <i className="material-icons">home</i>
+          </a>
+        </li>
+        <li className="m-3">
+          <i className="material-icons">groups</i>
+        </li>
+        <li className="m-3">
+          <i className="material-icons">dashboard</i>
+        </li>
+        <li className="m-3">
+          <i className="material-icons">manage_accounts</i>
+        </li>
+        <li className="m-3">
+          <a href="/logout">
+            <i className="material-icons">keyboard_return</i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default MobileSidebar

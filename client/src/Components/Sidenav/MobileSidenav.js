d83cbbb560cd5930a69b35c8340d4727df43d272
Kat Buchanan <katrbuch@gmail.com>
2021-05-05 17:15:12 -0500

    state/triggers for mobile

--- client/src/Components/Sidenav/MobileSidenav.js
+++ client/src/Components/Sidenav/MobileSidenav.js
@@ -1,30 +1,55 @@
 import React from 'react'
 import MobileUser from '../User/MobileUser'
+import BrowseUsersModal from '../Modals/BrowseUsersModal'
+import UpdateProfileModal from '../Modals/UpdateProfileModal'
+import ManageAccountModal from '../Modals/ManageAccountModal'
 import Logo from '../../assets/img/logo.png'
 
 function MobileSidenav() {
+  const [usersModalShow, setUsersModalShow] = React.useState(false)
+  const [profileModalShow, setProfileModalShow] = React.useState(false)
+  const [accountModalShow, setAccountModalShow] = React.useState(false)
+
   return (
     <div id="mobile-side-nav">
       <img src={Logo} style={{ width: '50px' }} alt="devlr logo"></img>
       <ul className="list-group">
-        <li className="m-3">
-          <a href="/home">
+        <li>
+          <button type="button" href="/home">
             <i className="material-icons">home</i>
-          </a>
+          </button>
         </li>
-        <li className="m-3">
-          <i className="material-icons">groups</i>
+        <li>
+          <button type="button" onClick={() => setUsersModalShow(true)}>
+            <i className="material-icons">groups</i>
+          </button>
         </li>
-        <li className="m-3">
-          <i className="material-icons">dashboard</i>
+        <BrowseUsersModal
+          show={usersModalShow}
+          onHide={() => setUsersModalShow(false)}
+        />
+        <li>
+          <button type="button" onClick={() => setProfileModalShow(true)}>
+            <i className="material-icons">dashboard</i>
+          </button>
         </li>
-        <li className="m-3">
-          <i className="material-icons">manage_accounts</i>
+        <UpdateProfileModal
+          show={profileModalShow}
+          onHide={() => setProfileModalShow(false)}
+        />
+        <li>
+          <button type="button" onClick={() => setAccountModalShow(true)}>
+            <i className="material-icons">manage_accounts</i>
+          </button>
+          <ManageAccountModal
+            show={accountModalShow}
+            onHide={() => setAccountModalShow(false)}
+          />
         </li>
-        <li className="m-3">
-          <a href="/logout">
+        <li>
+          <button type="button" href="/logout">
             <i className="material-icons">keyboard_return</i>
-          </a>
+          </button>
         </li>
       </ul>
       <MobileUser />

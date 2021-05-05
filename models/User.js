7ad98efe957f5d592a4c0c25d6e7d3d15f4d962e
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-04 22:39:36 -0500

    added and removed dotenv. Just wanted to test atlas db connection

--- models/User.js
+++ models/User.js
@@ -35,8 +35,8 @@ User.prototype.validPassword = (password) =>
 
 // Hooks are automatic methods that run during various phases of the Users Model lifecycle
 // In this case, before a Users is created, we will automatically hash their password
-User.addHook('beforeCreate', (user) => {
-  User.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
-})
+// User.addHook('beforeCreate', (user) => {
+//   User.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+// })
 
 module.exports = User

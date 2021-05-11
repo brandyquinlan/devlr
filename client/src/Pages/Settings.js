82a5667293e4e5752472773b76e858d29cd959d4
Kat Buchanan <katrbuch@gmail.com>
2021-05-10 21:37:41 -0500

    settings page styling

--- client/src/Pages/Settings.js
+++ client/src/Pages/Settings.js
@@ -3,6 +3,7 @@ import { Redirect, useLocation } from 'react-router-dom'
 import { Spinner, Button } from 'react-bootstrap'
 import API from '../utils/API'
 import ResetPasswordModal from '../Components/Modals/ResetPasswordModal'
+import DeleteAccountModal from '../Components/Modals/DeleteAccountModal'
 
 function useQuery() {
   return new URLSearchParams(useLocation().search)
@@ -12,6 +13,7 @@ export default function Settings() {
   const [authenticating, setAuthenticating] = useState(true)
   const [authenticated, setAuthenticated] = useState(false)
   const [resetPasswordModal, setResetPasswordModal] = useState(false)
+  const [deleteAccountModal, setDeleteAccountModal] = useState(false)
   const resetCode = useQuery().get('reset')
   const [user, setUser] = useState({})
 
@@ -52,30 +54,56 @@ export default function Settings() {
   }, [resetCode])
 
   return (
-    <div>
+    <div id="settings">
       {authenticating ? (
         <Spinner animation="border" />
       ) : (
         [
           authenticated === true ? (
-            <div
-              className="d-flex flex-row align-items-top justify-content-around"
-              id="col1"
-            >
-              <div className="d-flex flex-column align-items-left" id="col2">
+            <div className="d-flex flex-column align-items-start">
+              <a href="/home" className="mb-2">
+                <span className="material-icons">west</span>Go Back
+              </a>
+              <div className="tab gradient">
+                <h4>
+                  Account Settings{' '}
+                  <span className="material-icons" style={{ fontSize: '26px' }}>
+                    manage_accounts
+                  </span>
+                </h4>
+                <hr />
+                <p className="small" id="accountMsg">
+                  Use the buttons below to manage your devlr account settings.
+                  These actions have no effect on your linked GitHub account.
+                </p>
+                <div className="separator mt-4"></div>
                 <ResetPasswordModal
                   show={resetPasswordModal}
                   onHide={() => setResetPasswordModal(false)}
                   user={user}
                 />
-                <Button onClick={() => setResetPasswordModal(true)}>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="gradient"
+                  onClick={() => setResetPasswordModal(true)}
+                >
                   Reset Password
                 </Button>
+                <DeleteAccountModal
+                  show={deleteAccountModal}
+                  onHide={() => setDeleteAccountModal(false)}
+                  user={user}
+                />
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="gradient ml-2"
+                  onClick={() => setDeleteAccountModal(true)}
+                >
+                  Delete Account
+                </Button>
               </div>
-              <div
-                className="d-flex flex-column align-items-right ml-4"
-                id="col3"
-              ></div>
             </div>
           ) : (
             <Redirect to="/login" />

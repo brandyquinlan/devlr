bc55ea8910a47059a4a1baabb747ce2c8cba7483
Kat Buchanan <katrbuch@gmail.com>
2021-05-12 17:02:27 -0500

    modal content, needs fn

--- client/src/Components/Modals/DeleteAccountModal.js
+++ client/src/Components/Modals/DeleteAccountModal.js
@@ -11,15 +11,38 @@ function DeleteAccountModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Delete Account
+          Delete Account{' '}
+          <span className="material-icons" style={{ fontSize: '26px' }}>
+            person_remove
+          </span>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div>Actions</div>
+        <div
+          className="gradient"
+          style={{
+            border: 'none',
+            borderRadius: '20px',
+            padding: '2rem',
+            margin: '0',
+          }}
+        >
+          <h5>We&apos;re sad to see you go!</h5>
+          <hr />
+          <p>Are you sure you want to delete this devlr account?</p>
+          <br />
+          <p className="small">
+            This change is irreversible, but you can always create a new
+            account.
+          </p>
+        </div>
       </Modal.Body>
       <Modal.Footer>
+        <Button variant="secondary" className="gradient">
+          Delete and Logout
+        </Button>
         <Button variant="secondary" onClick={props.onHide}>
-          Close
+          Go Back
         </Button>
       </Modal.Footer>
     </Modal>

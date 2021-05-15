71198bd61ee8deea0c700af878ea4ae3587cb69b
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-15 09:19:39 -0500

    like post db route is working

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,7 +20,7 @@ router.put('/likePost', (request, response) => {
   // ALONG with the user who liked it, property userID
   const { like, postID } = request.body
   try {
-    db.Post.findOneAndUpdate({ id: postID }, { $push: { likes: like } }).then(
+    db.Post.findOneAndUpdate({ _id: postID }, { $push: { likes: like } }).then(
       (res) => {
         response.send(res)
       },

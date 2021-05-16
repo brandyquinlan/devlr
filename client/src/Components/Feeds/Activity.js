fff3a8eec34dcc708e7a2f270d20110f6f1dc54d
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-16 12:40:27 -0500

    Moved create comment function into the comment modal

--- client/src/Components/Feeds/Activity.js
+++ client/src/Components/Feeds/Activity.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import PostContainer from '../Posts/PostContainer'
 
-function Activity({ posts, createComment, createPost }) {
+function Activity({ posts, createPost }) {
   return (
     <div>
       {/* {loadingData ? (
         <PostContainer>Please wait while we get your posts...</PostContainer>
       ) : ( */}
-      <PostContainer
-        posts={posts}
-        createPost={createPost}
-        createComment={createComment}
-      />
+      <PostContainer posts={posts} createPost={createPost} />
       {/* )} */}
     </div>
   )

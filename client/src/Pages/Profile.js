44df4d7277e7f4889c2fe9ee0caba30ef77b7995
Kat Buchanan <katrbuch@gmail.com>
2021-05-17 14:34:23 -0500

    updated profile view

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -3,11 +3,13 @@ import Tab from '../Components/Tab'
 import useViewport from '../utils/useViewport'
 import Sidenav from '../Components/Sidenav/Sidenav'
 import MobileSidenav from '../Components/Sidenav/MobileSidenav'
-import ProfileNavbar from '../Components/Nav/ProfileNavbar'
+import Navbar from '../Components/Nav/Navbar'
+import NoExpandTab from '../Components/NoExpandTab'
+import FeaturedDevs from '../Components/FeaturedDevs/FeaturedDevs'
 
 function Profile() {
   const { width } = useViewport()
-  const breakpoint = 768
+  const breakpoint = 875
 
   return (
     <div className="container">
@@ -17,10 +19,12 @@ function Profile() {
       >
         {width < breakpoint ? <MobileSidenav /> : <Sidenav />}
         <div className="d-flex flex-column align-items-left" id="col2">
-          <ProfileNavbar />
+          <Navbar />
         </div>
         <div className="d-flex flex-column align-items-right ml-4" id="col3">
-          <Tab title="Featured Devs" />
+          <NoExpandTab title="Featured Devs">
+            <FeaturedDevs />
+          </NoExpandTab>
           <Tab title="Ad" />
         </div>
       </div>

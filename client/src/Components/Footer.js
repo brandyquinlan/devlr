3b71eb3cb3b69932dc8946617613d9fd2c4817bb
Kat Buchanan <katrbuch@gmail.com>
2021-05-17 13:49:24 -0500

    updated footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -3,20 +3,20 @@ import Logo from '../assets/img/logo.png'
 
 function Footer() {
   return (
-    <footer className="bg-secondary p-3">
-      <div className="float-left mx-3">
-        <img src={Logo} style={{ width: '40px' }} alt="devlr logo"></img>
-      </div>
+    <footer className="d-flex flex-row align-items-center justify-content-center py-2 mt-4">
       <div>
-        <p>Show your skills, share your life.</p>
-        <p>
+        <img src={Logo} style={{ width: '50px' }} alt="devlr logo"></img>
+      </div>
+      <div className="ml-2">
+        <p className="h6 m-0">Show your skills, share your life.</p>
+        <small>
           Connect with the devlr team on{' '}
-          <a href="https://github.com/brandyquinlan/devlr" target="_blank">
+          <a href="https://github.com/brandyquinlan/devlr" target="_blank" className="footerLink">
             {' '}
             GitHub{' '}
           </a>
-          or shoot us an <a href="mailto:devlrteam@gmail.com"> email </a>
-        </p>
+          or shoot us an <a href="mailto:devlrteam@gmail.com" className="footerLink"> email </a>
+        </small>
       </div>
     </footer>
   )

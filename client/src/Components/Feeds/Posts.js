191d55e87760e7775eda81ba9f073069fa8d3686
Kat Buchanan <katrbuch@gmail.com>
2021-05-17 14:32:53 -0500

    renamed activity -> posts

--- client/src/Components/Feeds/Posts.js
+++ client/src/Components/Feeds/Posts.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import PostContainer from '../Posts/PostContainer'
 
-function Posts() {
-  return <PostContainer />
+function Activity({ posts, createPost }) {
+  return (
+    <div>
+      {/* {loadingData ? (
+        <PostContainer>Please wait while we get your posts...</PostContainer>
+      ) : ( */}
+      <PostContainer posts={posts} createPost={createPost} />
+      {/* )} */}
+    </div>
+  )
 }
 
-export default Posts
+export default Activity

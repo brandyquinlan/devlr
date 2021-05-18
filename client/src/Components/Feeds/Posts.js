1e9c20038e46108779e3d1c09ee5731ad09ed2c3
Kat Buchanan <katrbuch@gmail.com>
2021-05-17 20:17:54 -0500

    consolidating post feed

--- client/src/Components/Feeds/Posts.js
+++ client/src/Components/Feeds/Posts.js
@@ -1,16 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PostContainer from '../Posts/PostContainer'
+import { TargetUserContext } from '../../utils/TargetUserState'
 
-function Posts({ posts, createPost }) {
-  return (
-    <div>
-      {/* {loadingData ? (
-        <PostContainer>Please wait while we get your posts...</PostContainer>
-      ) : ( */}
-      <PostContainer posts={posts} createPost={createPost} />
-      {/* )} */}
-    </div>
-  )
+function Posts() {
+  const [targetUser, targetDispatch] = useContext(TargetUserContext)
+
+  return targetUser._id ? <PostContainer home={false} /> : <PostContainer />
 }
 
 export default Posts

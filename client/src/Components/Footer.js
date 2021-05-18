6c61b389e102b6b222e5a36e8f4c3a87a06d28b9
Kat Buchanan <katrbuch@gmail.com>
2021-05-18 17:50:13 -0500

    pin class, rm margins

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -3,12 +3,12 @@ import Logo from '../assets/img/logo.png'
 
 function Footer() {
   return (
-    <footer className="footer d-flex flex-row align-items-center justify-content-center py-2 mt-4">
+    <footer className="footerPin d-flex flex-row align-items-center justify-content-center">
       <div className="ml-2">
         <img src={Logo} style={{ width: '50px' }} alt="devlr logo"></img>
       </div>
       <div className="ml-2">
-        <p className="h6 m-0">Show your skills, share your life.</p>
+        <p className="h6 m-0">Show your skills. Share your life.</p>
         <small>
           Connect with the devlr team on{' '}
           <a href="https://github.com/brandyquinlan/devlr" target="_blank" className="underlineLink">

c49139b0939b458d07e2dfec01e3de0e313ce222
Kat Buchanan <katrbuch@gmail.com>
2021-05-17 20:11:47 -0500

    rm extra feed file

--- client/src/Components/Feeds/ProfilePosts.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PostContainer from '../Posts/PostContainer'
-
-function ProfilePosts({ posts }) {
-  return <PostContainer posts={posts} home={false} />
-}
-
-export default ProfilePosts

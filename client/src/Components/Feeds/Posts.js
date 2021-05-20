da20f0fea61cb7753e33a7debd36359054d401cb
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-19 22:20:55 -0500

    how to set up a switch case for the call for posts in the socket handler

--- client/src/Components/Feeds/Posts.js
+++ client/src/Components/Feeds/Posts.js
@@ -10,8 +10,9 @@ export default function Posts({ home }) {
   const [store, dispatch] = useContext(UserContext)
   const [postStore, postDispatch] = useContext(PostContext)
   const [targetUser, targetDispatch] = useContext(TargetUserContext)
-
   const [room, setRoom] = useState('')
+
+  const homeOrAway = home ? 'home' : 'away'
 
   useEffect(() => {
     targetUser?.profile?.user
@@ -22,9 +23,20 @@ export default function Posts({ home }) {
   }, [store, targetUser])
 
   socket.once('update to feed', () => {
-    API.getFollowingPosts(store.user._id).then((res) => {
-      postDispatch({ type: 'set posts', payload: res })
-    })
+    switch (homeOrAway) {
+      case 'home':
+        API.getFollowingPosts(store.user._id).then((res) => {
+          postDispatch({ type: 'set posts', payload: res })
+        })
+        break
+      case 'away':
+        API.getPosts(targetUser.profile.user).then((res) => {
+          postDispatch({ type: 'set posts', payload: res })
+        })
+        break
+      default:
+        throw new Error()
+    }
   })
 
   return <PostContainer home={home} />
